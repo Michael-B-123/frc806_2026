a0894f207b9094256e8797b21e09e102b7d8b251
Michael-B-123 <mbulkin1179@gmail.com>
2026-02-27 02:35:12 +0000

    Started ball moving code(indexer+shooter), didn't do intake

--- src/main/java/frc/robot/Subsystems/Indexer.java
+++ src/main/java/frc/robot/Subsystems/Indexer.java
@@ -1,17 +1,33 @@
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class Indexer extends SubsystemBase {
+    private final SparkFlex bottomRoller;
+    private final SparkFlex topRoller;
     public Indexer(int bottomRollerMotorId, int topRollerMoterId) {
+        bottomRoller = new SparkFlex(bottomRollerMotorId, MotorType.kBrushless);
+        topRoller = new SparkFlex(topRollerMoterId, MotorType.kBrushless);
 
+        SparkFlexConfig config = new SparkFlexConfig();
+        config.idleMode(IdleMode.kCoast).smartCurrentLimit(30);
+
+        bottomRoller.configure(config, SparkFlex.ResetMode.kResetSafeParameters, SparkFlex.PersistMode.kPersistParameters);
+        topRoller.configure(config, SparkFlex.ResetMode.kResetSafeParameters, SparkFlex.PersistMode.kPersistParameters);
     }
 
     // No default command
 
     public Command index() {
         // Spin motors at correct duty cycles. We should not need controllers.
-        return runOnce(() -> {});
+        return runOnce(() -> {
+            bottomRoller.set(0.4);
+            topRoller.set(0.4);
+        }).withName("Start Indexing");
     }
 
     @Override
--- src/main/java/frc/robot/Subsystems/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -1,8 +1,21 @@
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class Shooter extends SubsystemBase {
+    private final SparkFlex shooter;
+    public Shooter(int shooterMotorId, int topRollerMoterId) {
+        shooter = new SparkFlex(shooterMotorId, MotorType.kBrushless);
+
+        SparkFlexConfig config = new SparkFlexConfig();
+        config.idleMode(IdleMode.kCoast).smartCurrentLimit(30);
+
+        shooter.configure(config, SparkFlex.ResetMode.kResetSafeParameters, SparkFlex.PersistMode.kPersistParameters);
+    }
     public Shooter(int motorId) {
 
     }
@@ -12,13 +25,13 @@ public class Shooter extends SubsystemBase {
 
     public Command prime() {
         // Default command, rotate slowly to reduce shooting prep time
-        return runOnce(() -> {});
+        return runOnce(() -> shooter.set(0.1)).withName("Prime");
     }
 
     public Command shoot() {
         // Speed up rollers, control velocty
         // https://docs.wpilib.org/en/stable/docs/software/advanced-controls/introduction/tuning-flywheel.html
-        return runOnce(() -> {});
+        return runOnce(() -> shooter.set(0.9)).withName("Shoot");
     }
 
     @Override
