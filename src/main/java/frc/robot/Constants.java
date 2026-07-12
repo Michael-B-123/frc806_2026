7b485ccf4e3591b0df3c5633257e74efc00c9253
Michael-B-123 <mbulkin1179@gmail.com>
2026-07-12 16:19:31 +0000

    Change shooter speed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -96,7 +96,7 @@ public class Constants {
 
     public interface Shooter {
         public static final int MotorID = 17;
-        public static final double ShootRPM = 3500;
+        public static final double ShootRPM = 2300;
         public static final double kP = 0.5;
         public static final double kI = 0;
         public static final double kD = 0;
